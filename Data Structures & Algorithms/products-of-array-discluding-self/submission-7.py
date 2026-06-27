2182bd88f21f218635db285954d21e13a4d898ed
Rachel Yu <rachelyu0406@gmail.com>
2026-06-26 21:28:58 -0400

    Add: products-of-array-discluding-self - submission-7

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-7.py
@@ -0,0 +1,17 @@
+# For each index i, answer = product of everything before i * product of everything after i.
+# prefix[i] stores product before i.
+# suffix[i] stores product after i.
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        p = [1] * (len(nums) + 1)
+        s = [1] * (len(nums) + 1)
+        res = [0] * len(nums)
+        for i in range(len(nums)):
+            p[i + 1] = p[i] * nums[i]
+        for i in range(len(nums) - 1, -1, -1):
+            s[i - 1] = s[i] * nums[i]
+        for i in range(len(nums)):
+            res[i] = p[i] * s[i]
+
+        return res
