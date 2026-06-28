65522d60be1dfeb062e682654feb9e2da8027e5f
Rachel Yu <rachelyu0406@gmail.com>
2026-06-28 14:16:04 -0400

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.py
@@ -0,0 +1,17 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+# use intervals.sort(key=lambda interval: interval.start)
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda interval: interval.start)
+        for i in range(len(intervals) - 1):
+            print(intervals[i].end, intervals[i + 1].start)
+            if intervals[i].end > intervals[i + 1].start:
+                return False
+        return True
